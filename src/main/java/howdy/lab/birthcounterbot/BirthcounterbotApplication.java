9a1f49c761fbd55453722a29792f3a6ee91a8a1b
Incognito Bot Dev <dev@incognito-bot.local>
2026-02-26 09:49:20 +0500

    feat: initial commit

---
+++ src/main/java/howdy/lab/birthcounterbot/BirthcounterbotApplication.java
@@ -0,0 +1,13 @@
+package howdy.lab.birthcounterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthcounterbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthcounterbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/howdy/lab/birthcounterbot/BirthcounterbotApplicationTests.java
@@ -0,0 +1,13 @@
+package howdy.lab.birthcounterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class BirthcounterbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
